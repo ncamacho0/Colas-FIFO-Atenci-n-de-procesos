a7c6db27c10947d107a2b820a83e480718c3b7dc
ncamacho0 <ncamacho0@ucol.mx>
2017-06-04 13:04:57 -0500

    Colas FIFO :: Atención de procesos
    Colas FIFO :: Atención de procesos

---
+++ Colas FIFO/Colas FIFO/Procesador.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas_FIFO
+{
+    class Procesador
+    {
+        private Proceso inicio=null, final=null;
+        private int tProcesos = 0, ciclosNecesarios = 0;
+        //Agregar
+        public void push(Proceso p)
+        {
+            if (inicio != null)
+            {
+                final.siguente = p;
+                final = p;
+            }
+            else
+            {
+                inicio = p;
+                final = p;               
+            }
+            tProcesos++;
+            ciclosNecesarios += p.ciclosProceso;
+        }
+        //Eliminar
+        public void pop(int ciclosRestantes)
+        {
+            if (ciclosRestantes == 0)
+            {
+                inicio = inicio.siguente;
+                tProcesos--;
+            }
+        }
+
+        public int procesar()
+        {
+            int restantes;
+            restantes = inicio.ciclosRestantes();
+            ciclosNecesarios--;
+            return restantes;
+        }
+
+        public int totalProcesosFinal()
+        {
+            return tProcesos;
+        }
+
+        public int ciclosNesesarios()
+        {
+            return ciclosNecesarios;
+        }
+        //total
+        public Proceso verificarProcesos()
+        {
+            return inicio;
+        }
+
+   
+    }
+}
---
+++ Colas FIFO/Colas FIFO/Proceso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas_FIFO
+{
+    class Proceso
+    {
+        public Proceso siguente { get; set; }
+        static Random miRandom = new Random();
+        private int _ciclosProceso;
+        public int ciclosProceso { get { return _ciclosProceso; } }
+
+        public Proceso()
+        {
+            _ciclosProceso = miRandom.Next(4, 15);
+            siguente = null;
+        }
+
+        public int ciclosRestantes()
+        {
+            _ciclosProceso--;
+            return _ciclosProceso;
+        }
+
+        public override string ToString()
+        {
+            return "Ciclos necesarios: " + _ciclosProceso + Environment.NewLine;
+        }
+    }
+}
